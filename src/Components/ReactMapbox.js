3d065e8f320d61cb5bdd519ee83502c2dbc308ce
Santosh Rai <santoshrai@Santoshs-MacBook-Pro.local>
2020-04-19 16:21:51 +0900

    change to map => foreach donot return anything

--- src/Components/ReactMapbox.js
+++ src/Components/ReactMapbox.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 
 import React, { useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 import './style.css'
 
 import * as data from '../data/get-latest.json';
+import * as skate_data from '../data/skate-board.json';
 
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
@@ -22,42 +23,65 @@ export default function Map() {
 
     // const places =[];
     const places = data.places;
+    
 
     return (
-        <ReactMapGL
-            {...viewport}
-            mapStyle="mapbox://styles/mapbox/dark-v10"
-            onViewportChange={setViewport}
-        >
+        <div>
+            <ReactMapGL
+                {...viewport}
+                mapStyle="mapbox://styles/mapbox/dark-v10"
+                onViewportChange={setViewport}
+            >
+                {skate_data.features.map(park =>
+                    // const square = function(number) { return number * number }
+                    (
+                        <Marker key={park.properties.PARK_ID} longitude={park.geometry.coordinates[0]} latitude={park.geometry.coordinates[1]}>
+                            <button className="marker-btn">
+                                {/* <img src="../logo.svg"></img> */}
+                                {/* <div>Find Me</div> */}
+                                {/* {console.log(currentPlace.longitude)} */}
+                            </button>
+                        </Marker>
 
-            { data.reports
-                .filter(report => !report.hide)
-                .forEach(report => {
-                    const { infected, placeId } = report;
-                    const currentPlace = places.find(place => place.id == placeId);
-                    // console.log(currentPlace);
-                    // new mapboxgl.Marker({
-                    //     // color: getColorFromCount(infected)
-                    //     color:"grey"
-                    // })
+                    )
+                )
+                }
+
+
+                {data.reports
+                    .filter(report => !report.hide)
+                    .map(report => {
+                        const { infected, placeId } = report;
+                        const currentPlace = places.find(place => place.id == placeId);
+                        // currentPlaces
+                        // console.log(currentPlace);
+                        // new mapboxgl.Marker({
+                        //     // color: getColorFromCount(infected)
+                        //     color:"grey"
+                        // })
                         // .setLngLat([currentPlace.longitude, currentPlace.latitude])
                         // .addTo(map);
                         // console.log(currentPlace.longitude);
-                    return (<Marker key={placeId} longitude={currentPlace.longitude} latitude={currentPlace.latitude}>
-                        <button className="marker-btn">
-                            {/* <img src="../logo.svg"></img> */}
-                            <div>Find Me</div>
-                        </button>
+                    
+                     
+                            // <Marker key={placeId} longitude={currentPlace.longitude} latitude={currentPlace.latitude}>
+                            (<Marker  longitude={138.25293} latitude={36.204823}>
 
-                    </Marker>)
+                                <button className="marker-btn">
+                                    {/* <img src="../logo.svg"></img> */}
+                                    {/* <div>Find Me</div> */}
+                                    {/* {console.log(currentPlace.longitude)} */}
+                                </button>
 
-                })
+                            </Marker>
+                            )
+
+                    })
+                }
 
 
-            }
-
-
-        </ReactMapGL>
+            </ReactMapGL>
+        </div>
     );
-        
+
 }
